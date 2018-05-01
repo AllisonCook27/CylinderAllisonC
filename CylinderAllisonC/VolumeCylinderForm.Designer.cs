6bfde26ac8de87d1a0e7ee6a8c52056850bd32d4
Allison <allison.cook@ocsbstudent.ca>
2018-05-01 12:35:47 -0400

    Initial commit

---
+++ CylinderAllisonC/VolumeCylinderForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace CylinderAllisonC
+{
+    partial class frmVolumeCylinder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblRadius = new System.Windows.Forms.Label();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeight.ForeColor = System.Drawing.Color.White;
+            this.lblHeight.Location = new System.Drawing.Point(43, 49);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(123, 23);
+            this.lblHeight.TabIndex = 0;
+            this.lblHeight.Text = "Enter a height :";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(181, 49);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(111, 160);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(88, 33);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblRadius
+            // 
+            this.lblRadius.AutoSize = true;
+            this.lblRadius.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRadius.ForeColor = System.Drawing.Color.White;
+            this.lblRadius.Location = new System.Drawing.Point(43, 93);
+            this.lblRadius.Name = "lblRadius";
+            this.lblRadius.Size = new System.Drawing.Size(124, 23);
+            this.lblRadius.TabIndex = 3;
+            this.lblRadius.Text = "Enter a radius :";
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(181, 93);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 20);
+            this.txtRadius.TabIndex = 4;
+            // 
+            // frmVolumeCylinder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
+            this.ClientSize = new System.Drawing.Size(320, 261);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.lblRadius);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.lblHeight);
+            this.Name = "frmVolumeCylinder";
+            this.Text = "Volume of a Cylinder by Allison C";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblRadius;
+        private System.Windows.Forms.TextBox txtRadius;
+    }
+}
+
---
+++ CylinderAllisonC/VolumeCylinderForm.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Created by: Allison Cook
+ * Created on: 25 April, 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #31 - Volume of a Cylinder
+ * This program calculates the volume of a cylinder with height and radius given by user
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CylinderAllisonC
+{
+    public partial class frmVolumeCylinder : Form
+    {
+        public frmVolumeCylinder()
+        {
+            InitializeComponent();
+        }
+
+        private double CalculateVolume (double height, double radius)
+        {
+            //declare variable
+            double area;
+
+            //return the area value
+            return area;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //delare variables
+
+        }
+    }
+}
